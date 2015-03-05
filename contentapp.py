67a573e6903ed6faf8fb5d0d978aa11b6929420b
Jesus Alberto Saiz <ja.saizc@alumnos.urjc.es>
2015-03-05 10:37:47 +0100

    Añadidas funcionalidades

--- contentapp.py
+++ contentapp.py
@@ -26,8 +26,20 @@ class contentApp (webapp.webApp):
 
     def parse(self, request):
         """Return the resource name (including /)"""
-
-        return request.split(' ', 2)[1]
+        
+        recurso = request.split(' ', 2)[1]
+        metodo = request.split(' ', 2)[0]
+        if metodo == "PUT":
+            cuerpo == request.split("\r\n\r\n", 1)[1]
+        elif metodo == "POST":
+            print "HOLA" + request.split("\r\n\r\n", 1)[1]
+            cuerpo == request.split("\r\n\r\n", 1)[1]
+        else:
+            cuerpo = ""
+        return (metodo, recurso, cuerpo)
+        
+        
+        
 
     def process(self, resourceName):
         """Process the relevant elements of the request.
@@ -35,14 +47,33 @@ class contentApp (webapp.webApp):
         Finds the HTML text corresponding to the resource name,
         ignoring requests for resources not in the dictionary.
         """
-
-        if resourceName in self.content.keys():
+        metodo, recurso, cuerpo = resourceName
+        if metodo == "GET":        
+            if recurso in self.content.keys():
+                httpCode = "200 OK"
+                htmlBody = "<html><body>" + self.content[recurso] \
+                + "<form id='formulario' method='post'><fieldset><legend>Formulario de Ejemplo</legend><label>Recurso</label> \                    <input id='campo1' name='recurso' type='text' /><label>push</label><input id='campo2' name='push' type='text' /><input id='campo3' name='enviar' type='submit'\ value='Enviar' /></fieldset></form>"+ "</body></html>"
+            else:
+                httpCode = "404 Not Found"
+                htmlBody = "Not Found"
+        elif metodo == "PUT":
+            elf.content[recurso] = cuerpo
             httpCode = "200 OK"
-            htmlBody = "<html><body>" + self.content[resourceName] \
+            htmlBody = "<html><body>" + recurso + "Vale Ahora" + self.content[recurso] \
                 + "</body></html>"
-        else:
-            httpCode = "404 Not Found"
-            htmlBody = "Not Found"
+            
+        elif metodo == "POST":
+            self.content[recurso] = cuerpo
+            httpCode = "200 OK"
+            htmlBody = "<html><body>" + recurso + "Vale Ahora" + self.content[recurso] \
+                    + "</body></html>"
+            
+        elif metodo == "DELETE":   
+            self.content[recurso] = cuerpo
+            httpCode = "200 OK"
+            htmlBody = "<html><body>" + recurso + "Vale Ahora" + self.content[recurso] \
+                    + "</body></html>"
+                
         return (httpCode, htmlBody)
 
 
